907d2f4c6967d22ea418de56c587d9646d55fe49
Dan1Almeida <daniel.dnlsilva7@gmail.com>
2025-02-10 19:46:40 -0300

    Primeiro

---
+++ AppContatos/AppContatos/src/main/java/br/com/dnl/AppContatos/repository/PessoasRepository.java
@@ -0,0 +1,13 @@
+package br.com.dnl.AppContatos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.dnl.AppContatos.model.Pessoas;
+
+@Repository
+public interface PessoasRepository extends JpaRepository<Pessoas, Long> {
+	
+	// VERIFICAR MAIORES CONSULTAS PERSONALIZADAS
+	
+}
